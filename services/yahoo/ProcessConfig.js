53f384243a438c80dc282d7a9ae11abaecdff17f
Balaji <balajitheblankde@gmail.com>
2026-03-28 16:10:14 +0530

    change : JSON to js object

--- services/yahoo/ProcessConfig.js
+++ services/yahoo/ProcessConfig.js
@@ -1,14 +1,12 @@
-const fs = require("fs");
+const { Companies } = require("../../config/Yahoo/Top500Companies");
 
 class ProcessConfig {
   getSymbolsTop500Companies() {
-    const rawData = fs.readFileSync(
-      "config/Yahoo/Top500Companies.json",
-      "utf-8",
-    );
-    const data = JSON.parse(rawData);
+    const rawData = Companies;
 
-    const listOfCompanies = data.data;
+ 
+
+    const listOfCompanies = rawData.data;
 
     const symbols = listOfCompanies
       .filter((stock) => stock.symbol !== "NIFTY 500") // remove index
